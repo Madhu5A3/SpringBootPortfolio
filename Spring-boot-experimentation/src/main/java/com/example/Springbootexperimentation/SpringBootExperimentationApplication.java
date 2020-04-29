e75d082ca252073ae0d5bdcb7f3e494d849fc880
Madhu Kiran Varma <madhuv299@gmail.com>
2020-04-29 22:14:16 +0530

    Spring boot experimentaion code (#1)
    * spring-dependecy-injection
    
    * string experimentation
    
    * CRUD operations using SpringBoot
    
    * springboot code for web using jsp
    
    * SpringBoot Experimentation code
    
    Co-authored-by: madhumudunuri <madhumudunur@e0284l.lsz.ads.valuelabs.net>

---
+++ Spring-boot-experimentation/src/main/java/com/example/Springbootexperimentation/SpringBootExperimentationApplication.java
@@ -0,0 +1,13 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExperimentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExperimentationApplication.class, args);
+		System.out.println("Spring Boot Experimentation");
+	}
+
+}
